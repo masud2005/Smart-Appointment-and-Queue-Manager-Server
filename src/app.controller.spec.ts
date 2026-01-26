61d0655d8891b205ce2a37e259f771845fe3b256
masud2005 <masud20012005@gmail.com>
2026-01-26 12:58:21 +0600

    root api endpoint res change

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "Smart Appointment & Queue Manager API Server Running..."', () => {
+      expect(appController.getHello()).toBe('Smart Appointment & Queue Manager API Server Running...');
     });
   });
 });
